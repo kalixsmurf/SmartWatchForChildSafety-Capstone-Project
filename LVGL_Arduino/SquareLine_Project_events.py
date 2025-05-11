9bae759d6162897c43dfa637aab949150ab0241c
kalixsmurf <onat.oz@metu.edu.tr>
2025-05-11 13:21:41 +0300

    Finalized UI project in Arduino IDE

---
+++ LVGL_Arduino/SquareLine_Project_events.py
@@ -0,0 +1,32 @@
+
+def CheckPassword(event_struct):
+    return
+
+
+def LoadPredictionTableValues(event_struct):
+    return
+
+
+def LoadProfileInformation(event_struct):
+    return
+
+
+def SaveProfileInformationCallback(event_struct):
+    return
+
+
+def UpdateLockScreenTime(event_struct):
+    return
+
+
+def SaveSamplingRateToFile(event_struct):
+    return
+
+
+def LoadSamplingRateValues(event_struct):
+    return
+
+
+def ClearSamplingRateTable(event_struct):
+    return
+
